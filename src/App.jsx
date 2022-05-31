f98e5f641bf439cd465f1e6a28083ab6ef968408
ghalitsar coder <ghalitsardev@gmail.com>
2022-05-31 22:04:02 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-4xl font-bold">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/images/images.js
@@ -0,0 +1,31 @@
+import avatar from "./avatar.jpg";
+import avatar2 from "./avatar2.jpg";
+import avatar3 from "./avatar3.png";
+import avatar4 from "./avatar4.jpg";
+import product1 from "./product1.jpg";
+import product2 from "./product2.jpg";
+import product3 from "./product3.jpg";
+import product4 from "./product4.jpg";
+import product5 from "./product5.jpg";
+import product6 from "./product6.jpg";
+import product7 from "./product7.jpg";
+import product8 from "./product8.jpg";
+import product9 from "./product9.jpg";
+import welcomeBg from "./welcome-bg.svg";
+
+export default {
+  avatar,
+  avatar2,
+  avatar3,
+  avatar4,
+  product1,
+  product2,
+  product3,
+  product4,
+  product5,
+  product6,
+  product7,
+  product8,
+  product9,
+  welcomeBg,
+};
